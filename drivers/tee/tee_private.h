c3f8be3d53113b7527a349be9ccfa695f3edcf74
Amirreza Zarrabi <amirreza.zarrabi@oss.qualcomm.com>
2025-08-20 16:38:49 -0700

    tee: add close_context to TEE driver operation
    The tee_context can be used to manage TEE user resources, including
    those allocated by the driver for the TEE on behalf of the user.
    The release() callback is invoked only when all resources, such as
    tee_shm, are released and there are no references to the tee_context.
    
    When a user closes the device file, the driver should notify the
    TEE to release any resources it may hold and drop the context
    references. To achieve this, a close_context() callback is
    introduced to initiate resource release in the TEE driver when
    the device file is closed.
    
    Relocate teedev_ctx_get, teedev_ctx_put, tee_device_get, and
    tee_device_get functions to tee_core.h to make them accessible
    outside the TEE subsystem.
    
    Reviewed-by: Sumit Garg <sumit.garg@oss.qualcomm.com>
    Tested-by: Neil Armstrong <neil.armstrong@linaro.org>
    Tested-by: Harshal Dev <quic_hdev@quicinc.com>
    Signed-off-by: Amirreza Zarrabi <amirreza.zarrabi@oss.qualcomm.com>

--- drivers/tee/tee_private.h
+++ drivers/tee/tee_private.h
@@ -14,12 +14,6 @@
 
 int tee_shm_get_fd(struct tee_shm *shm);
 
-bool tee_device_get(struct tee_device *teedev);
-void tee_device_put(struct tee_device *teedev);
-
-void teedev_ctx_get(struct tee_context *ctx);
-void teedev_ctx_put(struct tee_context *ctx);
-
 struct tee_shm *tee_shm_alloc_user_buf(struct tee_context *ctx, size_t size);
 struct tee_shm *tee_shm_register_user_buf(struct tee_context *ctx,
 					  unsigned long addr, size_t length);
