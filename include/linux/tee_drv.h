3b9215d1bbf61000cf255778725e8ad65533523e
KancyJoe <kancy2333@outlook.com>
2025-10-07 09:10:41 +0000

    Revert "tee: add TEE_IOCTL_PARAM_ATTR_TYPE_UBUF"
    This reverts commit 9c20380e9eba39d9727e9a1c322709f819c69949.
    
    (cherry picked from commit b1f8f913ccbff8a159103610f87d098f0985aecc)

--- include/linux/tee_drv.h
+++ include/linux/tee_drv.h
@@ -82,11 +82,6 @@ struct tee_param_memref {
 	struct tee_shm *shm;
 };
 
-struct tee_param_ubuf {
-	void __user *uaddr;
-	size_t size;
-};
-
 struct tee_param_value {
 	u64 a;
 	u64 b;
@@ -97,7 +92,6 @@ struct tee_param {
 	u64 attr;
 	union {
 		struct tee_param_memref memref;
-		struct tee_param_ubuf ubuf;
 		struct tee_param_value value;
 	} u;
 };
